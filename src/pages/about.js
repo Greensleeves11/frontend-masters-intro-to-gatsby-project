8bed4e403e0504cec865472c1942f7cdea91e4f6
Sebastian Wegner <swegner@Sebastians-MacBook-Pro.local>
2024-01-14 17:08:37 +0100

    layout

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,15 @@
 import * as React from 'react';
+import { Link } from 'gatsby';
+import Layout from '../components/layout';
 
 export default function AboutPage() {
   return (
-    <main>
+    <Layout
+      title="About this site"
+      description="More information about this site"
+    >
       <h1>About this site</h1>
-    </main>
-  )
+      <Link to="/">Back to home</Link>
+    </Layout>
+  );
 }
